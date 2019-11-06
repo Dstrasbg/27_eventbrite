3877cdebe85f9b535ee182b29c73a42dea971ee0
dstrasbg <argudo.daniel@gmail.com>
2019-11-06 01:47:15 +0100

    css

--- db/migrate/20191105233822_add_devise_to_users.rb
+++ db/migrate/20191105233822_add_devise_to_users.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class AddDeviseToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def self.up
     change_table :users do |t|
-      ## ALREADY CREATE IN THE ORIGINAL TABLE : Database authenticatable
+      ## Database authenticatable
       #t.string :email,              null: false, default: ""
       #t.string :encrypted_password, null: false, default: ""
 
@@ -34,7 +34,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
@@ -43,5 +43,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
     # add_index :users, :unlock_token,         unique: true
   end
 
-
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
 end
